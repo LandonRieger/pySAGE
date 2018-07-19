79147fdffb8aaf332e0dc2680fe5038bf519a5f2
Landon Rieger <landon.rieger@usask.ca>
2018-07-19 15:56:47 -0700

    add readme to setup for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+from os import path
 from setuptools import setup, find_packages
 import versioneer
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='pysagereader',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       description='SAGE binary file readers',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
       author='USASK ARG',
       author_email='landon.rieger@usask.ca',
       license='MIT',
