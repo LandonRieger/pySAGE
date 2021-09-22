e88330224c2a58f2e0d07adb5e1fe83ebf434e2b
LandonRieger <landon.rieger@gmail.com>
2021-09-22 10:02:47 -0700

    get rid of SAGEIIILoader reference

--- pysagereader/__init__.py
+++ pysagereader/__init__.py
@@ -1,5 +1,4 @@
 from pysagereader.sage_ii_reader import SAGEIILoaderV700
-from pysagereader.sage_iii_reader import SAGEIIILoaderV400
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
